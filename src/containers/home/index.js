105d2a0888be1e1af1f375d45cd7b8da805cbaf8
uraway <masato.uraway@gmail.com>
2017-08-26 10:58:00 +0800

    Fix eslint errors

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Home = props => (
+const Home = (props) => (
   <div>
     <h1>Home</h1>
     <p>Welcome home!</p>
@@ -11,7 +12,11 @@ const Home = props => (
   </div>
 );
 
-const mapDispatchToProps = dispatch => bindActionCreators({
+Home.propTypes = {
+  changePage: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
   changePage: () => push('/about-us'),
 }, dispatch);
 
